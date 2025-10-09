9b9d95062876e718926cc1708b7baa454bb160ec
ArefOrumiehei <areforumiehei@gmail.com>
2025-10-09 19:20:53 +0330

    Create Items page

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,16 @@ import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
 import ProtectedRoute from './protecting/ProtectedRoute';
 import StoreDetails from './components/tables/StoreDetails';
-import ItemsTable from './components/tables/ItemsTable';
+import StoreTagsDetails from './components/tables/StoreTagsDetails';
 import Stores from './pages/dashboard/Stores';
+import ForgotPasswordPage from './pages/auth/ForgotPasswordPage';
+import ResetPasswordPage from './pages/auth/ResetPasswordPage';
+import Home from './pages/Home';
+import Items from './pages/dashboard/Items';
 
 
 function TagsPage() {
   return <div>Tags page</div>;
-}
-
-function ItemsPage() {
-  return (
-    <div>
-      <ItemsTable />
-    </div>
-  );
 }
 
 function App() {
@@ -28,12 +24,16 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
 
         <Route path="/" element={<ProtectedRoute><DashboardLayout /></ProtectedRoute>}>
+          <Route index element={<Home />} />
           <Route path="stores" element={<Stores />} />
           <Route path="stores/:id" element={<StoreDetails />} />
+          <Route path="stores/:id/tags" element={<StoreTagsDetails />} />
+          <Route path="items" element={<Items />} />
           <Route path="tags" element={<TagsPage />} />
-          <Route path="items" element={<ItemsPage />} />
         </Route>
         <Route path="login" />
       </Routes>
---
+++ src/pages/dashboard/Items.tsx
@@ -0,0 +1,11 @@
+import ItemsTable from "@/components/tables/ItemsTable"
+
+function Items() {
+  return (
+    <div>
+      <ItemsTable />
+    </div>
+  )
+}
+
+export default Items
