67898f57482d231aabf3ddbb1ad69759b12af0d3
ArefOrumiehei <areforumiehei@gmail.com>
2025-10-09 19:28:48 +0330

    Update types

--- src/types/storeTypes.ts
+++ src/types/storeTypes.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export type Item = {
-  store: any; id: number; name: string; price: number 
+  store: any; id: number; name: string; price: number; description: string;
 };
 export type Tag = { id: number; name: string };
+export type Tags = { id: number; name: string, items?: Item[]};
 export type Store = { id: number; name: string; items: Item[]; tags: Tag[] };
